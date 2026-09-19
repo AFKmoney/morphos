6bb1d955820968bd31d0034531b8d9111bda6285
Canaddons <philippeantoine1996@gmail.com>
2026-09-19 02:47:45 -0400

    chore: remove Mistral P3 slop (analytics index)

--- src/lib/analytics/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Advanced Analytics System
- * 
- * Système complet d'analyse et de suivi pour MorphOS
- */
-
-export * from './analytics-types';
-export * from './analytics-engine';
-export * from './analytics-store';
